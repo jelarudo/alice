5298459f51c66071b54da3a7dc4e7c4f002e78d2
Dinh Thuy Duong <dinh.thuy.duong@nifty.co.jp>
2014-11-29 22:08:09 +0900

    Add crystal and score

---
+++ AliceInPaperLand/Assets/juon/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+		public static int score; 
+		static int highScore;
+		GameObject crystal;
+		// Use this for initialization
+		void Start ()
+		{
+				crystal = GameObject.Find ("Crystal");
+				crystal.SetActive (false);
+				highScore = PlayerPrefs.GetInt ("highScore", 0);
+		}
+	
+		void OnDestroy ()
+		{
+				PlayerPrefs.SetInt ("highScore", highScore);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (score > highScore) {
+						highScore = score;
+				}
+				guiText.text = "Score :" + score + "\n HighScore" +
+						highScore;
+				if (Score.score >= 10) {
+						crystal.SetActive (true);
+				}
+		}
+}
